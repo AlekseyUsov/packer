596d3611d1e54808f3d0301e1f5da1f850505adb
Aleksey Usov <beckett@protonmail.ch>
2017-09-04 23:00:31 +0300

    proxy support added

--- provisioners/centos7-provisioner.sh
+++ provisioners/centos7-provisioner.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
 USER_HOME=/home/vagrant
-SSH_DIR=$USER_HOME/.ssh
+SSH_DIR=${USER_HOME}/.ssh
+SSH_AUTH_KEYS=/tmp/authorized_keys
+PROXY_HOST=$1
+PROXY_PORT=$2
+PROXY=${PROXY_HOST}:${PROXY_PORT}
 
-# Installing EPEL and additional packages
+# Install EPEL and additional useful packages
 yum -y install epel-release
 yum -y install htop
 
 # Setting up .ssh directory for vagrant user
-if [ ! -d $SSH_DIR ]; then
-  mkdir -pm 0700 $SSH_DIR
+if [ ! -d ${SSH_DIR} ]; then
+  mkdir -pm 0700 ${SSH_DIR}
 fi
 
-mv /tmp/authorized_keys $SSH_DIR
-chown -R vagrant:vagrant $SSH_DIR && chmod +x $SSH_DIR
+if [ -f ${SSH_AUTH_KEYS} ]; then
+  mv ${SSH_AUTH_KEYS} ${SSH_DIR}
+  chown -R vagrant:vagrant ${SSH_DIR} && chmod +x ${SSH_DIR}
+fi
 
 # Adding vagrant user to the sudoers file
 echo "vagrant	ALL=(root) NOPASSWD:ALL" >> /etc/sudoers
 echo "UseDNS no" >> /etc/ssh/sshd_config
+
+# Setting up proxy, if needed
+if [ ! -z "${PROXY_HOST}" ]; then
+  echo -e proxy="http://${PROXY}\n" >> /etc/yum.conf
+  echo -e "\nexport http_proxy=http://${PROXY}" >> /etc/profile
+  echo -e "export https_proxy=https://${PROXY}\n" >> /etc/profile
+fi
 
 # Setting Message of the Day to the time of the build
 echo -e "\nBuilt via packer (https://www.packer.io) at `date`\n" > /etc/motd
